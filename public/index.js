31c8b694a5627ae4c39b02e24d34e7d0201bcc53
Kazak Dmitry <kazak.dmitry1989@mail.ru>
2016-12-26 16:07:22 +0200

    add functionality to data_list

--- public/index.js
+++ public/index.js
@@ -8,46 +8,22 @@ angular.module("app",["btford.socket-io",'LocalStorageModule','ui.mask'])
 		})
 		.controller("ctrl", function($scope,mySocket,localStorageService){
 			
-	//теперь $localstorage
-				
-				/*localStorageService.set('key',[{
-							id:1,
-							first_name:'Ivan',
-							last_name:'Pupkin',
-							sex:0,
-							bdate:"12.12.1990",
-							source:0,
-							phones:[
-								"+375(33)123-22-33",
-								"+375(29)222-33-55"
-							],
-							communication:[
-								{
-									date:"25.12.2016",
-									text:"Пообщались на тему покупки оборудования"
-								},
-								{
-									date:"27.12.2016",
-									text:"Сказал не будет покупать ничего"
-								}
-								
-							]},
-							{
-								id:2,
-								first_name:"Ivan",
-								last_name:"Ivanov",
-								sex:0,
-								bdate:"05.12.1990",
-								source:2,
-								phones:[],
-								communication:[]
-							}]);
-			var k = localStorageService.get('key');
-			k.push(3);*/
+	
+			
+			$scope.showInfo = function(item,event){
+				var t = angular.element(event.target.parentNode.parentNode.parentNode)[0];
+				var s=angular.element(t).children();
+				for ( var i=0; i<s.length; i++ ){
+					 if(angular.element(s[i].children).hasClass('clicked')){
+					 	angular.element(s[i].children).removeClass('clicked');
+					 }
+				}
+				$scope.commun = item.communication;
+				angular.element(event.target.parentNode).addClass('clicked');
+			};
 
-			//$scope.info="info";
-			
-			
+
+
 			if(localStorageService.get('take')==null){
 				$scope.db=[{
 							id:1,
@@ -91,13 +67,13 @@ angular.module("app",["btford.socket-io",'LocalStorageModule','ui.mask'])
 
     //socket.io
     //$scope.name = '';
-			$scope.$watch('name',function(){
+			/*$scope.$watch('name',function(){
 				mySocket.emit('data', $scope.name);
 				mySocket.on('data', function(data){
 					console.log(data);
 					$scope.name = data;
 				});	
-			});
+			});*/
 
 				
 
